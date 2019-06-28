b900d74652301afdbace4caf3157508316bbb30c
Julian Dorsey <dorseyj@elsevier.com>
2019-06-28 19:00:11 -0400

    create.py added

--- create.py
+++ create.py
@@ -1,35 +1,39 @@
-#!/Users/ma-dorseyj/homebrew/miniconda3/bin/python3
+#!<path to python3 installation>
 
-## imports
+# imports
 import os
 import subprocess
 from github import Github
-from sys import argv
+import sys
 
-## grab repository name from stdin
-repo_name = argv[1]
-user = argv[2]
+sys.path.append('./.config/')
+from git_config import access_token
 
-g = Github("4a2d678cf68d886e8d5fbb1350d643ba4d4f75cf")
-print('Not enough aguments') if not (len(argv) == 2) else print('Creating {} repository'.format(repo_name))
+# error handling
+print('Not enough aguments') if not (len(sys.argv) == 3) else print('Creating {} repository'.format(sys.argv[1]))
 
-## set path and url variables // change directory to new proj
+# grab repository name from stdin
+repo_name = sys.argv[1]
+user = sys.argv[2]
+g = Github(access_token)
+
+# set path and url variables // change directory to new proj
 path = '/Users/{}/Documents/dev/proj/{}'.format(os.environ.get('USER'), repo_name)
 os.mkdir(path)
 os.chdir(path)
 
-# ## touch README and populate with repo name
+# touch README and populate with repo name
 readme = open('README.md', 'w')
 readme.write('# {}'.format(repo_name.capitalize()))
 readme.close()
 
-## init git
+# init git
 subprocess.run(['git', 'init'])
 
-## github request
+# github request
 g.get_user().create_repo(name=repo_name, private=True)
 
-## git add origin and push
+# git add origin and push
 url = 'https://github.com/{}/{}.git'.format(user, repo_name)
 subprocess.run(['git', 'remote', 'add', 'origin', '{}'.format(url)])
 subprocess.run(['git', 'add', '*'])
